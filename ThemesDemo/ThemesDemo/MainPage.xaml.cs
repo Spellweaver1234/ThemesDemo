93e055d5e72e214de50b14cb1cffb313f2cda15b
Арсений Зорин <>
2023-04-21 15:25:05 +0700

    1

---
+++ ThemesDemo/ThemesDemo/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Themes;
+
+namespace ThemesDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public enum Themes
+        {
+            Dark,
+            Light
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            SwitchTheme(Themes.Light);
+
+        }
+
+        private void SwitchTheme(Themes theme)
+        {
+            var resources = App.Current.Resources;
+
+            switch (theme)
+            {
+                case Themes.Dark:
+                    resources.MergedWith = typeof(DarkThemeResources);
+                    break;
+                case Themes.Light:
+                    resources.MergedWith = typeof(LightThemeResources);
+                    break;
+            }
+        }
+
+        void Button_Clicked_1(System.Object sender, System.EventArgs e)
+        {
+            SwitchTheme(Themes.Dark);
+        }
+    }
+}
+
